347f8b0415b3e67b2cf39fb38970f4cbe7499610
MeHasDagger <39961892+MeHasDagger@users.noreply.github.com>
2019-01-06 13:59:24 +0100

    Javadoc to Solver

--- src/Solver.java
+++ src/Solver.java
@@ -1,11 +1,25 @@
 import java.util.List;
-
+/**
+ * This class solves the maze by finding the correct path
+ * 
+ * @author Mattias Melchior, Sanna Lundqvist
+ *
+ */
 public class Solver {
 	
-	public static boolean findPath(int[][] maze, int x, int y, List<Integer> correctPath) {
+	/**
+	 * This is a recursive class that sends out branches of itself in every direction to determine the way to the end.
+	 * 
+	 * @param maze - the maze
+	 * @param x - the x-coordinate
+	 * @param y - the y-coordinate
+	 * @param pathList - the path needed to take to reach the end
+	 * @return true if a way is found, false if standing on a wall or already visited grid
+	 */
+	public static boolean findPath(int[][] maze, int x, int y, List<Integer> pathList) {
 		if (maze[x][y] == 9) {  // Slutdestinationen
-			correctPath.add(y);
-			correctPath.add(x);
+			pathList.add(y);
+			pathList.add(x);
 			
 			return true;
 		} else if (maze[x][y] == 0) {  // Icke besökt ruta	
@@ -13,36 +27,36 @@ public class Solver {
 			 
 			 int dx = 0;
 			 int dy = -1;
-			 if (findPath(maze, x + dx, y + dy, correctPath)) {  // Kollar på rutan ovanför
-				 correctPath.add(y);
-				 correctPath.add(x);
+			 if (findPath(maze, x + dx, y + dy, pathList)) {  // Kollar på rutan ovanför
+				 pathList.add(y);
+				 pathList.add(x);
 				 
 				 return true;
 			 }
 			 
 			 dx = 1;
 			 dy = 0;
-			 if (findPath(maze, x + dx, y + dy, correctPath)) {  // Kollar på rutan höger 
-				 correctPath.add(y);
-				 correctPath.add(x);
+			 if (findPath(maze, x + dx, y + dy, pathList)) {  // Kollar på rutan höger 
+				 pathList.add(y);
+				 pathList.add(x);
 				
 				 return true;
 			 }
 			 
 			 dx = 0;
 			 dy = 1;
-			 if (findPath(maze, x + dx, y + dy, correctPath)) {  // Kollar på rutan under 
-				 correctPath.add(y);
-				 correctPath.add(x);
+			 if (findPath(maze, x + dx, y + dy, pathList)) {  // Kollar på rutan under 
+				 pathList.add(y);
+				 pathList.add(x);
 			
 				 return true;
 			 }
 			
 			 dx = -1;
 			 dy = 0;
-			 if (findPath(maze, x + dx, y + dy, correctPath)) {  // Kollar på rutan vänster
-				 correctPath.add(y);
-				 correctPath.add(x);
+			 if (findPath(maze, x + dx, y + dy, pathList)) {  // Kollar på rutan vänster
+				 pathList.add(y);
+				 pathList.add(x);
 			
 				 return true;
 			 }
